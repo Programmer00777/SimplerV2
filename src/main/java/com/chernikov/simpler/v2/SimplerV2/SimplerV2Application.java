e2bf5096982f06e3f2b464bc52065498d736cf0d
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-02-25 23:40:42 +0200

    Optimized for mobile version. Added new navbar.

--- src/main/java/com/chernikov/simpler/v2/SimplerV2/SimplerV2Application.java
+++ src/main/java/com/chernikov/simpler/v2/SimplerV2/SimplerV2Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableWebMvc
 public class SimplerV2Application {
 
 	public static void main(String[] args) {
