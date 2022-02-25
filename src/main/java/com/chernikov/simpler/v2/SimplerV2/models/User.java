39fcea03cfcbf7823b44539b45c3816a5151aa15
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-02-26 00:35:00 +0200

    Changed database + changes in User.class

--- src/main/java/com/chernikov/simpler/v2/SimplerV2/models/User.java
+++ src/main/java/com/chernikov/simpler/v2/SimplerV2/models/User.java
@@ -1,9 +1,6 @@
 package com.chernikov.simpler.v2.SimplerV2.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -11,19 +8,23 @@ import javax.validation.constraints.Size;
 
 
 @Entity
+@Table(name = "user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Min(value = 0, message = "ID cannot be a negative number")
+	@Column(name = "id")
 	private Integer id;
 	
 	@NotNull(message="A name cannot be empty")
 	@Size(min = 2, max = 30, message="The length of the name must be between 2 and 30 characters")
+	@Column(name = "name")
 	private String name;
 	
 	@NotNull(message="An email cannot be empty")
 	@Email(message="You have entered an incorrect email")
+	@Column(name = "email")
 	private String email;
 	
 	public User() {}
